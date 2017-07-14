dc64cd1c0809d46bc1f0513b7add3fa72866b438
Johannes Fischer <johannes.fischer@rakuten.com>
2017-07-14 09:45:02 +0900

    add command line option to filter by floor

--- README.md
+++ README.md
@@ -1,6 +1,61 @@
-### Crimson House Menu
+# Crimson House Menu
 
-See what for lunch / dinner in Crimson House Tokyo
+See what's for lunch / dinner in Crimson House Tokyo.
+
+## Installation
+
+```bash
+npm install -g crimson-house-menu
+```
+
+## How to use
+
+Simply run: `crimson-house-menu`
+
+```bash
+$ crimson-house-menu
+
+Rakuten Crimson House Lunch Menu
+
+9F
+Main A     : Deep-fried chicken
+Main B     : Salt-grilled salmon
+Main B     : Stir-fried pork & eggplant with Kimuchi
+Grill      : "Jingisukan" (Grilled mutton)
+Bowl A     : Chicken & egg Bowl
+Bowl B     : Seafood Curry
+Pasta      : Spaghetti with tuna, asparagus & tomato cream sauce
+Udon & Soba: "Abura Udon" (Udon noodles with seasoning oil)
+Ramen      : Shyoyu Ramen Hachioji-style
+Halal      : Cream stew with chicken sausage
+
+22F
+Main A     : Cheese fritters (Aji & white fish) w/ wasabi & yogurt sauce
+Main B     : Simmered chicken w/ ponzu
+Main C     : Stir-fried beef w/ green onion & salt
+Ramen      : “Mazesoba” (Ramen w/ oil based sauce)
+Udon & Soba: Soba or udon Sanuki-style
+Udon & Soba: Cold udon or soba
+Grill      : Cheese in hamburger steak w/ demiglace sauce “Grill ver.”
+Bowl A     : Eggplants & poached egg w/ meat thick sauce on rice
+Bowl B     : BBQ chicken & curry “Jamaica-style”
+Pasta      : Spaghetti w/ spinach & salmon cream sauce
+```
+
+### Filters
+
+__Floor__
+
+Type: `number`
+
+Show the menu by floor.
+
+```bash
+# readable
+$ crimson-house-menu --floor 9
+# alias
+$ crimson-house-menu -f 22
+```
 
 ## TODO
 
@@ -9,3 +64,4 @@ See what for lunch / dinner in Crimson House Tokyo
   * floor
   * meal time
 * add display option compact / full
+* option to display images
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const fetch = require('node-fetch');
 const chalk = require('chalk');
+const commandLineArgs = require('command-line-args');
+// const imgcat = require('imgcat');
 const pad = require('pad');
 const apiUrl = 'http://rakuten-towerman.azurewebsites.net/towerman-restapi/rest/cafeteria/menulist?menuDate=';
+
+const optionDefinitions = [
+  { name: 'floor', alias: 'f', type: Number }
+];
+
+const options = commandLineArgs(optionDefinitions);
 
 function getDate() {
   const now = new Date();
@@ -11,10 +19,22 @@ function getDate() {
   return `${year}${month}${day}`;
 }
 
+function filterItems(items, mealTime) {
+  // meal time
+  items = items.filter((item) => item.mealTime === mealTime);
+
+  // floor
+  if (options.floor !== null && [9, 22].includes(options.floor)) {
+    items = items.filter((item) => item.cafeteriaId === `${options.floor}F`);
+  }
+
+  return items;
+}
+
 function displayMenu(body) {
   const mealTime = new Date().getHours() < 15 ? 1 : 2;
   const mealTimeTitle = mealTime === 1 ? 'Lunch' : 'Dinner';
-  const items = body.data.filter((item) => item.mealTime === mealTime);
+  const items = filterItems(body.data, mealTime);
   let floor = '';
 
   console.log(chalk.hex('#bf0000').bold.underline(`Rakuten Crimson House ${mealTimeTitle} Menu\n`));
@@ -24,9 +44,11 @@ function displayMenu(body) {
       if (floor !== '') {
         console.log('');
       }
+
       floor = item.cafeteriaId;
       console.log(chalk.bold.underline(floor));
     }
+
     console.log(`${pad(item.menuType, 11)}: ${item.title}`);
   });
 }
@@ -40,6 +62,7 @@ function fetchMenu() {
         console.error(chalk.red('ERROR:'), body.errorMessage);
         return;
       }
+
       displayMenu(body);
     });
 }
